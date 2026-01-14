51158d28345bdaf48a53e4a0a9818dabe995c190
fullycharged25 <gargishan2010@gmail.com>
2026-01-13 22:45:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import streamlit as st
 import pandas as pd
 datatable = pd.DataFrame()
+# Create page
 st.set_page_config(page_title = "Homework Tracker", page_icon = "📚")
 st.title("Homework Tracker")
-
+# Save previous submissions
 if "history" not in st.session_state:
     st.session_state.history = []
 
 assignment = st.text_area("Assignment Name: ")
 subject = st.text_area("Class Name: ")
-due = st.text_area("Due Date (Day/Month/Year): ")
+due = st.date_input("Due Date: ")
+# Ensure that the user has inputed something, adding information to list.
 if st.button("Add to Task List"):
-    if assignment.strip() == "" or subject.strip() == "" or due.strip() == "":
+    if assignment.strip() == "" or subject.strip() == "":
         st.warning("Please enter some text.")
     else:
         st.session_state.history.append({"Assignment" : assignment, "Subject" : subject, "Due Date" : due})
+        st.write("Task Added")
 
-# st.write(tasks)
+# Show the final table.
 if st.button("Show Task List"):
     st.subheader("Homework To Do In Order")
     datatable = pd.DataFrame(st.session_state.history)
+    # st.write(datatable)
+    datatable = datatable.sort_values("Due Date") # Sort the table
     st.dataframe(datatable)
